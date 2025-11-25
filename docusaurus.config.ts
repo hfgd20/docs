a17a1182f1bf97e75dcc3901f70f0f551825a76d
Marc Rufeis <github@craftycram.net>
2025-11-25 11:23:02 +0100

    fix base url

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -12,7 +12,7 @@ const config: Config = {
 	url: "https://v2.hfg.design",
 	// Set the /<baseUrl>/ pathname under which your site is served
 	// For GitHub pages deployment, it is often '/<projectName>/'
-	baseUrl: "/",
+	baseUrl: "/docs2/",
 
 	// GitHub pages deployment config.
 	// If you aren't using GitHub pages, you don't need these.
